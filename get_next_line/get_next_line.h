4b1fc5e75d1e9093c98231201b9664c15d7fe99d
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2021-12-20 16:27:48 +0100

    correct version of header file

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 16:06:30 by lgenevey          #+#    #+#             */
-/*   Updated: 2021/12/13 18:46:55 by lgenevey         ###   ########.fr       */
+/*   Updated: 2021/12/20 16:10:18 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@
 # include <fcntl.h>
 # include <stddef.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s, int ascii);
-int		has_symbol(char *str, int ascii);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strcpy(char *dest, char *src);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char *s, unsigned int start, size_t len);
-char	*ft_free(char *str);
+char    *get_next_line(int fd);
+size_t  ft_strlen(const char *s, int ascii);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strjoin(char *s1, char *s2);
+char    *ft_strdup(const char *s1);
+char    *ft_substr(char *s, unsigned int start, size_t len);
+char    *ft_free(char *str);
 
 #endif
