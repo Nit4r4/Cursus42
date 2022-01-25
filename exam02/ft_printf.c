f0a4b864fbbf7c705e5872c951aa664b13400d7b
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-01-25 15:51:32 +0100

    modif main on printf.c

--- exam02/ft_printf.c
+++ exam02/ft_printf.c
@@ -92,18 +92,22 @@ int ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	char	*s = "salut";
+	char	*s = "Hey";
 	int		d = -10;
+	int		fret = 0;
+	int		ret = 0;
 
 	printf("Salut\n");
 	printf("%%s : %s\n", s);
-	printf("%%d : %d\n", d);
+	ret = printf("%%d : %d\n", d);
 	printf("%%x : %x\n", d);
+	printf("ret printf: %d\n", ret);
 
 	ft_printf("Salut\n");
 	ft_printf("%%s : %s\n", s);
-	ft_printf("%%d : %d\n", d);
+	fret = printf("%%d : %d\n", d);
 	ft_printf("%%x : %x\n", d);
+	printf("ret ft_printf: %d\n", fret);
 
 	return (0);
 }
