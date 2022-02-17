19f1b2970478fc4fffda00a73ad15ebca7a34d82
Lucie Genevey <lgenevey@c1r7s1.42lausanne.ch>
2022-02-17 18:02:07 +0100

    add modif server.c

--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,17 +6,20 @@
 /*   By: lgenevey <lgenevey@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 15:59:03 by lgenevey          #+#    #+#             */
-/*   Updated: 2022/02/17 17:28:06 by lgenevey         ###   ########.fr       */
+/*   Updated: 2022/02/17 17:40:22 by lgenevey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
 
-
+/*
+	int sig : signal that needs to be handled
+*/
 void	sig_handler(int sig)
 {
-
+	if (sig == SIGUSR1 || sig == SIGUSR2)
+		ft_printf("correct signal sent");
 }
 
 int	main(void)
@@ -25,5 +28,6 @@ int	main(void)
 
 	pid = getpid();
 	ft_printf("%d\n", pid);
+	
 	return (0);
 }
